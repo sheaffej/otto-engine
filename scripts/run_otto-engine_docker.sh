4d5288dd62317e93072309671d4222cc4a98f9ac
sheaffej <sheaffej@gmail.com>
2020-09-27 02:49:37 +0000

    update run script for daemon mode

--- scripts/run_otto-engine_docker.sh
+++ scripts/run_otto-engine_docker.sh
@@ -7,12 +7,12 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker rm -f $CONTAINER_NAME
 
-docker run  -it \
+docker run  -d \
 --name $CONTAINER_NAME \
 --net host \
 -e TZ=$TZ \
 -v $MYDIR/../config.ini:/config/config.ini:ro \
--v /home/jsheaffe/otto-rules:/json_rules:rw \
+-v /home/jsheaffe/otto-rules:/app/json_rules:rw \
 --restart always \
 $IMAGE
 
